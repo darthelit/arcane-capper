11a7af1d4de7ea44e7af31ab3a4a5e6246ddde9b
darthelit <dvincent25@gmail.com>
2020-06-11 13:15:55 -0500

    Implement Recoil

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
 import React from 'react';
-import { Button, View } from 'react-native';
+import { Button, TextInput, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { LinearGradient } from 'expo-linear-gradient';
 
+/* Recoil */
+import { RecoilRoot, atom, selector, useRecoilState, useRecoilValue } from 'recoil';
+
+const textState = atom({
+  key: 'textState', // unique ID (with respect to other atoms/selectors)
+  default: 'Tyndi Tinkerings', // default value (aka initial value)
+});
+
 const Drawer = createDrawerNavigator();
 const Base = createStackNavigator();
 
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+  return <Text>Character Count: {count} </Text>;
+}
+
+const charCountState = selector({
+  key: 'charCountState', // uniqueId ( with respect to other atoms/selectors)
+  get: ({ get }) => {
+    const text = get(textState);
+    return text.length;
+  },
+});
+
 function HomeScreen({ navigation }) {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (value) => {
+    setText(value);
+  };
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#A330C9' }}>
       <LinearGradient
@@ -21,6 +48,14 @@ function HomeScreen({ navigation }) {
           height: 300,
         }}
       />
+      <View>
+        <TextInput
+          value={text}
+          onChangeText={onChange}
+          style={{ minWidth: 300, fontSize: 20, height: 45, backgroundColor: '#627C81', textAlign: 'center' }}
+        />
+        <CharacterCount />
+      </View>
       <Button onPress={navigation.openDrawer} title='Open navigation drawer' />
       <Button onPress={() => navigation.navigate('Notifications')} title='Go to notifications' />
     </View>
@@ -84,10 +119,12 @@ function Home() {
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Base.Navigator headerMode='none'>
-        <Base.Screen name='Home' component={Home} />
-      </Base.Navigator>
-    </NavigationContainer>
+    <RecoilRoot>
+      <NavigationContainer>
+        <Base.Navigator headerMode='none'>
+          <Base.Screen name='Home' component={Home} />
+        </Base.Navigator>
+      </NavigationContainer>
+    </RecoilRoot>
   );
 }
